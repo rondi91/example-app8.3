18b3646d4c253711314049dec13c6a89f7ce5dd8
rondi91 <rondie91@gmail.com>
2022-03-14 15:42:45 -0700

    tambah  make:migration CreateProductTagsTable

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -7,5 +7,43 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    use HasFactory;
+
+
+    public $table = 'tags';
+    protected $primaryKey = 'tag_id';
+
+    public $fillable = [
+        'tag_slug',
+        'tag_name'
+    ];
+
+    public $timestamps  = false;
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'tag_id' => 'integer',
+        'tag_slug' => 'string',
+        'tag_name' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'tag_slug' => 'required',
+        'tag_name' => 'required'
+    ];
+
+    /**
+     * @return BelongsToMany
+     */
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'product_tags', 'tag_id', 'product_id');
+    }
 }
--- database/migrations/2022_03_14_224030_create_product_tags_table.php
+++ database/migrations/2022_03_14_224030_create_product_tags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagsTable extends Migration
+class CreateProductTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
-            $table->bigIncrements('tag_id');
-            $table->string('tag_slug', 100)->unique();
-            $table->string('tag_name');
+        Schema::create('product_tags', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -27,6 +26,6 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('product_tags');
     }
 }
